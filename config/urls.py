cd389b5b4af87aafede8802cc23ed26eeaa7173d
Carlos Visbal <carlosvisbal66@gmail.com>
2023-11-06 18:40:59 -0500

    documentacion
    documentacion

--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,28 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from apps.posts.api.router import router_post
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Base API",
+      default_version='v1',
+      description="Base description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="visbal"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=False,
+   permission_classes=(permissions.IsAdminUser,),
+)
 
 urlpatterns = [
     path('config/admin/', admin.site.urls),
-    path('api/', include(router_post.urls))
+    path('api/', include(router_post.urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     
 ]
